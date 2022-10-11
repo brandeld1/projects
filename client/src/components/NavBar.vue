771ecd472fb1a21c4f04dead4033a59158808a46
brandeld1 <brandeld1@newpaltz.edu>
2022-10-11 00:54:45 -0400

    Client full working, added navbar

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { ref } from 'vue';
+
+  let isActive = ref(false);
+
+</script>
+<template>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img
+          src="https://bulma.io/images/bulma-logo.png"
+          width="112"
+          height="28"
+        />
+      </a>
+
+      <a
+        :class="{ 'is-active': isActive}"
+        @click="isActive =! isActive"
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': isActive}">
+      <div class="navbar-start">
+        <a class="navbar-item"> Workouts </a>
+
+        <a class="navbar-item"> Add/Edit </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link"> More </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item"> Friends </a>
+            <a class="navbar-item"> Statistics </a>
+            <a class="navbar-item"> Contact </a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item"> Report an issue </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light"> Log in </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ client/src/components/RecentWorkouts.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<style>
+    .columns{
+        text-align:center;
+        margin:auto;
+    }
+</style>
+
+<template>
+  <div class = "columns is-mobile">
+        <div class = "column">
+            123
+        </div>
+        <div class = "column">
+            456
+        </div>
+        <div class = "column">
+            789
+        </div>
+    </div>
+</template>
